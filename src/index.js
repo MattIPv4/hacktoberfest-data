e61e84b5082ad64985ded4d54c19c3bef8ddecdd
MattIPv4 <me@mattcowley.co.uk>
2019-11-06 20:35:15 +0000

    Break down repos by language

--- src/index.js
+++ src/index.js
@@ -56,15 +56,16 @@ const main = () => {
     // Breaking down PRs by language, other tags
     const PRsByLanguage = ValidPRs.groupBy(pr => pr.base.repo.language);
     console.log('');
-    console.log(`Total languages: ${Object.keys(PRsByLanguage).filter(k => !!k).length}`);
+    console.log(`PRs by language (${Object.keys(PRsByLanguage).length} languages)`);
     PRsByLanguage.forEach((key, val) => {
-        console.log(`  ${key} PRs: ${val.length} (${(val.length / totalPRs * 100).toFixed(2)}%)`);
+        key = key === 'null' ? 'Undetermined' : key;
+        console.log(`  ${key}: ${val.length} (${(val.length / totalPRs * 100).toFixed(2)}%)`);
     });
 
     // Lines of code per PR
     const changesPerPR = ValidPRs.map(pr => pr.changes()).sort().reverse();
     console.log('');
-    console.log(`Largest change in PR: ${changesPerPR[0]}`);
+    console.log(`Largest change in PR: ${changesPerPR[0]} changes`);
 
     /***************
      * User Stats
@@ -83,27 +84,56 @@ const main = () => {
     // "Connections made" First time PRs to a project, and not first time PRs
     // We only have relevant PR data, this would need massive abuse of the GH API to determine
 
+    // Breaking down repos by language
+    const ReposByLanguage = Repos.groupBy(repo => repo.language);
+    console.log('');
+    console.log(`Repos by language (${Object.keys(ReposByLanguage).length} languages)`);
+    ReposByLanguage.forEach((key, val) => {
+        key = key === 'null' ? 'Undetermined' : key;
+        console.log(`  ${key}: ${val.length} (${(val.length / Repos.length * 100).toFixed(2)}%)`);
+    });
+
     // Projects by popularity, contributors, stars (repo metadata)
     const topReposByStars = Repos.sort((a, b) => {
         return b.stargazers_count - a.stargazers_count;
     }).limit(5);
+    console.log('');
+    console.log('Top repos by stars');
+    topReposByStars.forEach(repo => {
+        console.log(`  ${repo.stargazers_count} | ${repo.html_url}`);
+    });
+
     const topReposByForks = Repos.sort((a, b) => {
         return b.forks_count - a.forks_count;
     }).limit(5);
+    console.log('');
+    console.log('Top repos by forks');
+    topReposByForks.forEach(repo => {
+        console.log(`  ${repo.forks_count} | ${repo.html_url}`);
+    });
+
     const topReposByWatchers = Repos.sort((a, b) => {
         return b.watchers_count - a.watchers_count;
     }).limit(5);
+    console.log('');
+    console.log('Top repos by watchers');
+    topReposByWatchers.forEach(repo => {
+        console.log(`  ${repo.watchers_count} | ${repo.html_url}`);
+    });
+
     const topReposByPRs = Repos.sort((a, b) => {
         return b.prs.length - a.prs.length;
-    }).limit(5);
-    const topReposByContributors = Repos.sort((a, b) => {
-        return b.contributors().length - a.contributors().length;
     }).limit(5);
     console.log('');
     console.log('Top repos by PRs');
     topReposByPRs.forEach(repo => {
         console.log(`  ${repo.prs.length} | ${repo.html_url}`);
     });
+
+    const topReposByContributors = Repos.sort((a, b) => {
+        return b.contributors().length - a.contributors().length;
+    }).limit(5);
+    console.log('');
     console.log('Top repos by contributors');
     topReposByContributors.forEach(repo => {
         console.log(`  ${repo.contributors().length} | ${repo.html_url}`);
